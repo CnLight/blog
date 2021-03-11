a421b0cf98fab9db5a0289ae9c99b9d2a0d94036
CnLight <252955074@qq.com>
2021-03-11 23:56:05 +0800

    second2

---
+++ src/main/java/com/wl/blog/controller/admin/ESController.java
@@ -0,0 +1,63 @@
+package com.wl.blog.controller.admin;
+
+import com.wl.blog.service.admin.ContentService;
+import com.wl.blog.util.LayUITableData;
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author wl
+ * @title: ESController
+ * @projectName blog
+ * @description: TODO
+ * @date 2021/3/1012:07
+ */
+@Controller
+public class ESController {
+    @Resource
+    private ContentService contentService;
+    @GetMapping("/parse/{keywords}")
+    @ResponseBody
+    public boolean parse(@PathVariable String keywords) throws Exception {
+        return contentService.addPage(keywords);
+    }
+
+
+    @GetMapping("/search/{keywords}/{pageNo}/{pageSize}")
+    @ResponseBody
+    public List<Map<String,Object>> search(@PathVariable String keywords , @PathVariable int pageNo ,
+                                           @PathVariable int pageSize) throws IOException {
+        return   contentService.searchPage(keywords, pageNo, pageSize,0);
+    }
+
+    @GetMapping("/searchLayui/{keywords}")
+    @ResponseBody
+    public JSONObject searchByLayUI(@PathVariable String keywords ) throws Exception {
+
+        return LayUITableData.TransDataToLayUITable(contentService.searchPageAll(keywords));
+    }
+
+    @RequestMapping("/toSearch")
+    public String toSearch(){
+        return "esearch";
+    }
+
+
+
+    @RequestMapping("/addBlogData/{articleName}/{articleContent}")
+    @ResponseBody
+    public String addBlogData(@PathVariable String articleName ,@PathVariable String articleContent){
+
+        return "";
+
+    }
+}
---
+++ src/main/java/com/wl/blog/service/admin/ContentService.java
@@ -0,0 +1,166 @@
+package com.wl.blog.service.admin;
+
+import com.alibaba.fastjson.JSON;
+import com.wl.blog.pojo.BlogArticle;
+import com.wl.blog.util.HtmlUtil;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.text.Text;
+import org.elasticsearch.common.unit.TimeValue;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.index.query.TermQueryBuilder;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightField;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author wl
+ * @title: ContentService
+ * @projectName study
+ * @description: TODO
+ * @date 2021/3/820:08
+ */
+@Service
+public class ContentService {
+
+    @Resource
+    private RestHighLevelClient restHighLevelClient;
+    //1解析数据放入es中
+    public Boolean paraContent(String keywords,int pageNo)  {
+        HtmlUtil htmlUtil = new HtmlUtil();
+        BulkResponse bulkResponse = null;
+        List<BlogArticle> contents = null;
+        try {
+            contents = htmlUtil.parseCSDN(keywords,pageNo);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        BulkRequest bulkRequest = new BulkRequest();
+            bulkRequest.timeout("2s");
+            for (int i = 0; i< contents.size(); i++) {
+                bulkRequest.add(new IndexRequest("csdn").source(JSON.toJSONString(contents.get(i)), XContentType.JSON));
+            }
+        try {
+            bulkResponse=  restHighLevelClient.bulk(bulkRequest, RequestOptions.DEFAULT);
+        } catch (IOException e) {
+            System.out.println("已导入完成");
+            return true;
+        }
+
+
+        return !bulkResponse.hasFailures();
+    }
+
+    //实现多重插入
+    public boolean addPage(String keywords) throws Exception {
+        HtmlUtil htmlUtil = new HtmlUtil();
+        int allPageNum = htmlUtil.findAllPageNum(keywords, 1);
+        int i = 1;
+        while (i < allPageNum) {
+            Boolean aBoolean = this.paraContent(keywords, i);
+            if (aBoolean == true) i++;
+            else break;
+        }
+        return true;
+    }
+
+    //实现搜索功能
+    public List<Map<String,Object>> searchPage (String keywords ,int pageNo , int pageSize,int pageFlag) throws IOException {
+
+        if(pageNo<=1){
+            pageNo =1;
+        }
+        //
+/*        SearchTemplateRequest request = new SearchTemplateRequest();
+        request.setRequest(new SearchRequest("posts"));
+
+        request.setScriptType(ScriptType.INLINE);
+        request.setScript(
+                "{" +
+                        "  \"query\": { \"match\" : { \"{{field}}\" : \"{{value}}\" } }," +
+                        "  \"size\" : \"{{size}}\"" +
+                        "}");
+
+        Map<String, Object> scriptParams = new HashMap<>();
+        scriptParams.put("field", "title");
+        scriptParams.put("value", "elasticsearch");
+        scriptParams.put("size", 5);
+        request.setScriptParams(scriptParams);*/
+        //条件搜索
+        SearchRequest searchRequest = new SearchRequest("csdn");
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+
+        //分页
+        searchSourceBuilder.from(pageNo);
+        searchSourceBuilder.size(pageSize);
+
+        //精确匹配
+        TermQueryBuilder termQueryBuilder1 = QueryBuilders.termQuery("articleName",keywords);
+        TermQueryBuilder termQueryBuilder2 = QueryBuilders.termQuery("articleContent", articleContent);
+        searchSourceBuilder.query(termQueryBuilder1);
+        searchSourceBuilder.query(termQueryBuilder2);
+        searchSourceBuilder.timeout(new TimeValue(60, TimeUnit.SECONDS));
+
+
+        //高亮
+        HighlightBuilder highlightBuilder = new HighlightBuilder();
+        highlightBuilder.field("articleName");
+        highlightBuilder.requireFieldMatch(false);//关闭多个高亮
+        highlightBuilder.preTags("<span style='color:red'>");
+        highlightBuilder.postTags("</span>");
+        searchSourceBuilder.highlighter(highlightBuilder);
+
+
+        //执行结果返回
+        searchRequest.source(searchSourceBuilder);
+        SearchResponse searchResponse = restHighLevelClient.search(searchRequest,RequestOptions.DEFAULT);
+        //解析结果
+        ArrayList<Map<String,Object>> list = new ArrayList<>();
+        for (SearchHit searchHit: searchResponse.getHits().getHits()
+             ) {
+
+            //获取高亮的字段
+            Map<String, HighlightField> highlightFields = searchHit.getHighlightFields();
+            //获取高亮的标题
+            HighlightField title = highlightFields.get("articleName");
+            //原来的结果
+            Map<String,Object> stringObjectMap = searchHit.getSourceAsMap();
+            //解析高亮的字段
+            if(title!=null){
+                Text[] fragments = title.fragments();
+                String n_title = "";
+                for(Text text : fragments){
+                    n_title +=text;
+                }
+                stringObjectMap.put("articleName",n_title);
+            }
+            list.add(stringObjectMap);
+
+        }
+        return list;
+    }
+
+    //转换为表格数据的接口
+    //实现搜索功能
+    public List<Map<String,Object>> searchPageAll (String keywords ) throws IOException {
+         return   this.searchPage(keywords,1,100,0);
+
+    }
+
+
+}
